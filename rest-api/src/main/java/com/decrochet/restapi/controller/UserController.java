8053aa26090f60712f44381a68c557ece761a567
Rashīd Tlenexkoyotl <tlenexkoyotl@gmail.com>
2023-11-05 18:32:50 -0600

    #2 U(UserController.java): Controller is now functional

--- rest-api/src/main/java/com/decrochet/restapi/controller/UserController.java
+++ rest-api/src/main/java/com/decrochet/restapi/controller/UserController.java
@@ -5,10 +5,9 @@ import com.decrochet.restapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -22,5 +21,16 @@ public class UserController {
         User savedUser = userService.save(user);
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.getAll();
+
+        if (users == null || users.size() < 1) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
